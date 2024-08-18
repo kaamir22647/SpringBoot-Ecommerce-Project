5f48c42411f05daf0245156e96c6dcfb0e921f4b
kaamir22647 <kaamir22647@gmail.com>
2024-08-19 02:01:52 +0530

    removing unused imports

--- src/main/java/com/ecommerce/project/controller/CategoryController.java
+++ src/main/java/com/ecommerce/project/controller/CategoryController.java
@@ -1,22 +1,14 @@
 package com.ecommerce.project.controller;
 
-import com.ecommerce.project.config.AppConfig;
 import com.ecommerce.project.config.AppConstant;
-import com.ecommerce.project.model.Category;
 import com.ecommerce.project.payload.CategoryDTO;
 import com.ecommerce.project.payload.CategoryResponse;
 import com.ecommerce.project.service.CategoryService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.server.ResponseStatusException;
-
-import java.nio.file.Path;
-import java.util.ArrayList;
-import java.util.List;
 
 
 @RestController
